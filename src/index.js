bcd8a263b1d56d895683bcc7f05a2c0a41385162
刘华瑞 <1066521901@qq.com>
2022-05-09 17:11:43 +0800

    feat: 手动实现丐版h函数

--- src/index.js
+++ src/index.js
@@ -16,31 +16,25 @@ const patch = init([
 ]);
 
 const container = document.getElementById("container");
+const btn = document.getElementById("btn");
+console.log(btn);
 
-const vnode = h(
-  "div#container.two.classes",
-  { on: { click: function () {} } },
-  [
-    h("span", { style: { fontWeight: "bold" } }, "This is bold"),
-    " and this is just normal text",
-    h("a", { props: { href: "/foo" } }, "I'll take you places!"),
-  ]
-);
-// Patch into empty DOM element – this modifies the DOM as a side effect
+const vnode = h("ul", {}, [
+  h("li", { key: 0 }, "li1"),
+  h("li", { key: 1 }, "li2"),
+  h("li", { key: 2 }, "li3"),
+  h("li", { key: 3 }, "li4"),
+]);
+
 patch(container, vnode);
 
-const newVnode = h(
-  "div#container.two.classes",
-  { on: { click: function () {} } },
-  [
-    h(
-      "span",
-      { style: { fontWeight: "normal", fontStyle: "italic" } },
-      "This is now italic type"
-    ),
-    " and this is still just normal text",
-    h("a", { props: { href: "/bar" } }, "I'll take you places!"),
-  ]
-);
-// Second `patch` invocation
-patch(vnode, newVnode); // Snabbdom efficiently updates the old view to the new state
+const vnode2 = h("ul", {}, [
+  h("li", { key: 3 }, "li4"),
+  h("li", { key: 2 }, "li3"),
+  h("li", { key: 0 }, "li1"),
+  h("li", { key: 1 }, "li2"),
+]);
+
+btn.onclick = function () {
+  patch(vnode, vnode2);
+};
---
+++ src/mysnabbdom/h.js
@@ -0,0 +1,24 @@
+import vnode from "./vnode";
+
+export default function (sel, data, c) {
+  if (arguments.length !== 3) {
+    throw Error("目前只能传入三个参数");
+  }
+  if (typeof c == "string" || typeof c == "number") {
+    return vnode(sel, data, undefined, c, undefined);
+  }
+  if (Array.isArray(c)) {
+    let children = [];
+    for (let i = 0; i < c.length; i++) {
+      if (typeof c[i] == "object" && c[i].hasOwnProperty("sel")) {
+        children.push(c[i]);
+      } else {
+        throw Error("传参有误");
+      }
+    }
+    return vnode(sel, data, children, undefined, undefined);
+  }
+  if (typeof c == "object" && c.hasOwnProperty("sel")) {
+    return vnode(sel, data, [c], undefined, undefined);
+  }
+}
